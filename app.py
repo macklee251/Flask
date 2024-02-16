3c5f05b4a8f9614b4d702704d06aabb6bc8508ac
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-16 09:41:51 -0300

    .

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-host = "localhost"
-port = 8080
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['TITLE'] = "Pydaria"
+Bootstrap(app)
 
 @app.route('/')
-def hello():
-    return "Hello World!" 
-
-app.run(host=host, port=port)
+def index():
+    products = ["pão francês", "pão de forma", "pão de queijo"]
+    return render_template("index.html", products=products)
